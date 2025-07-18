26da2d4426fa561e56a8e642fc0d0ccb84ce5f4d
Justine Dube <justinedube08@gmail.com>
2025-07-18 22:24:07 +0200

    week1

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userInput;
+
+        
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+
+        } while (userInput != 0);
+
+        
+        int sum = numbers.Sum();
+
+        
+        double average = numbers.Average();
+
+        
+        int max = numbers.Max();
+
+        
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+    
     }
 }
